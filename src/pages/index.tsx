bc6460c4825097c1ebf3fdd7d7235ebf019d686e
Ricardo Sandoval <i.s.ricardo.sandoval@gmail.com>
2022-11-29 14:17:44 -0600

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { HomePage } from 'components/pages'
 import { GalleryProvider } from 'contexts'
 
-const title = 'Construye la casa de tus sueños en la zona con mayor plusvalía de la Durango, Durango.';
+const title = 'Real Campestre Residencial. Construye la casa de tus sueños en la zona con mayor plusvalía de la Durango, Durango.';
 const description = 'Los mejores terrenos de Durango, ubicados en la zona con mayor pluslvalía de la ciudad.';
 
 export default function Home() {
